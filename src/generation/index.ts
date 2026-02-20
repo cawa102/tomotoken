be23f8dbe75fa67fc08c1507431bab50f4ae5243
河合恭佑 <kawaikyousuke@kawaikyousukenoMacBook-Pro.local>
2026-02-20 18:29:08 +0000

    feat: add barrel exports for template system

--- src/generation/index.ts
+++ src/generation/index.ts
@@ -1,3 +1,4 @@
 export { creatureDesignSchema, type CreatureDesign, type Part, type Expression } from "./schema.js";
 export { buildPrompt, type PromptInput } from "./prompt.js";
-export { generateCreatureDesign, type DesignRequest } from "./designer.js";
+export { getDesignContext, saveDesign, type DesignContext } from "./cli.js";
+export { humanoidTemplate, applyCustomization, customizationSchema, type Customization } from "./templates/index.js";
---
+++ src/generation/templates/index.ts
@@ -0,0 +1,3 @@
+export { humanoidTemplate } from "./humanoid.js";
+export { applyCustomization } from "./apply.js";
+export { customizationSchema, type Customization, type BodyTemplate, type TemplatePart } from "./types.js";
